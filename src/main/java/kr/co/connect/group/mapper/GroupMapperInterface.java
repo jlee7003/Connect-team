c42eebde743b14e8801f93eb5b4927699e3c9652
youjin <estele.choi@gmail.com>
2019-11-27 11:22:17 +0900

    Mapper Interface (myBatis 쿼리 처리 클래스)

---
+++ src/main/java/kr/co/connect/group/mapper/GroupMapperInterface.java
@@ -0,0 +1,11 @@
+package kr.co.connect.group.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+
+import kr.co.connect.group.beans.GroupDto;
+
+public interface GroupMapperInterface {
+
+	@Insert("insert into taskgroup(groupname, manager, writeday) values(#{groupname},#{manager},now()")
+	void insertData(GroupDto groupDtop);
+}
