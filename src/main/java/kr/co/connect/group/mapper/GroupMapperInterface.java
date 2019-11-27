0236625868dbff0c6a9ea5da1fa8b3c9f623efe2
youjin <estele.choi@gmail.com>
2019-11-27 11:40:12 +0900

    GroupMapperInterface

--- src/main/java/kr/co/connect/group/mapper/GroupMapperInterface.java
+++ src/main/java/kr/co/connect/group/mapper/GroupMapperInterface.java
@@ -7,5 +7,5 @@ import kr.co.connect.group.beans.GroupDto;
 public interface GroupMapperInterface {
 
 	@Insert("insert into taskgroup(groupname, manager, writeday) values(#{groupname},#{manager},now()")
-	void insertData(GroupDto groupDtop);
+	void insertData(GroupDto groupDtop);	
 }
