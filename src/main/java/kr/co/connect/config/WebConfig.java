7a29a60524bbb0cfaa0198c3f94fd7d4b0effcb8
youjin <estele.choi@gmail.com>
2019-11-27 17:38:25 +0900

    하위 @ Configuration 파일

--- src/main/java/kr/co/connect/config/WebConfig.java
+++ src/main/java/kr/co/connect/config/WebConfig.java
@@ -18,27 +18,28 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import kr.co.connect.group.controller.GroupController;
 import kr.co.connect.group.mapper.GroupMapperInterface;
 
+//@ComponentScan(basePackages = "*", useDefaultFilters = false, includeFilters = @ComponentScan.Filter(kr.co.connect.group.controller.GroupController.class))
 @Configuration
-@ComponentScan(basePackages = "kr.co.connect", useDefaultFilters = false, includeFilters = @ComponentScan.Filter(GroupController.class))
+@ComponentScan(basePackages = "kr.co.connect")
 @EnableAspectJAutoProxy
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	// js, css, img 등 리소스 파일의 URL 매핑 
+	// js, css, img 등 리소스 파일의 URL 매핑
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-	
-	// ViewResolver -> 설정 내용이 없으면 404 예외 발생 
+
+	// ViewResolver -> 설정 내용이 없으면 404 예외 발생
 	@Bean
 	public BeanNameViewResolver beanNameViewResolver() {
 		BeanNameViewResolver viewResolver = new BeanNameViewResolver();
-		viewResolver.setOrder(0); // 우선순위 
+		viewResolver.setOrder(0); // 우선순위
 		return viewResolver;
 	}
-	
+
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -47,10 +48,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setOrder(1);
 		return viewResolver;
 	}
-	
-	
+
 	// 여기서부터 Jdbc 관련 빈들
-	
+
 	@Bean
 	public DriverManagerDataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -60,22 +60,23 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		dataSource.setPassword("12345678");
 		return dataSource;
 	}
-	
-	// SqlSessionFactory : Jdbc 처리 클래스 사용 
+
+	// SqlSessionFactory : Jdbc 처리 클래스 사용
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DriverManagerDataSource dataSource) throws Exception {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
 //		sqlSessionFactoryBean.setMapperLocations("classpath:kr/co/connect/group/mapper/GroupMapperInterface.class");
-		
+
 		SqlSessionFactory sqlSession = sqlSessionFactoryBean.getObject();
 		return sqlSession;
 	}
-	
+
 	// GroupMapperInterface : 쿼리문을 처리하는 클래스 (myBatis)
 	@Bean
 	public MapperFactoryBean<GroupMapperInterface> getMapper(SqlSessionFactory sqlSession) {
-		MapperFactoryBean<GroupMapperInterface> mapperFactoryBean = new MapperFactoryBean<GroupMapperInterface>(GroupMapperInterface.class);
+		MapperFactoryBean<GroupMapperInterface> mapperFactoryBean = new MapperFactoryBean<GroupMapperInterface>(
+				GroupMapperInterface.class);
 		mapperFactoryBean.setSqlSessionFactory(sqlSession);
 		return mapperFactoryBean;
 	}
