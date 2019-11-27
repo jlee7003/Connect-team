69bdd4dc34757ebf31b96bc6088667c4c73fc279
youjin <estele.choi@gmail.com>
2019-11-27 16:27:08 +0900

    @ Controller 빈들을 등록하는 설정파일

--- src/main/java/kr/co/connect/config/WebConfig.java
+++ src/main/java/kr/co/connect/config/WebConfig.java
@@ -1,6 +1,6 @@
-package kr.co.connect.group.config;
+package kr.co.connect.config;
+// @Controller 빈들만 등록 가능한 설정파일 (계층구조 적용)
 
-//import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperFactoryBean;
@@ -10,20 +10,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import kr.co.connect.group.controller.GroupController;
 import kr.co.connect.group.mapper.GroupMapperInterface;
 
-// @EnableWebMvc : <mvc:annotation-driven /> 과 동일
-// DispatcherServlet 은 @Configuration 파일을 읽어서 내부적으로 스프링 컨테이너를 생성 (ApplicationContext)
-// web 에 필요한 빈들을 대부분 자동 생성 
-// 추가 설정 필요시, WebMvcConfigurerAdapter 클래스를 상속받고 오버라이드 
-
 @Configuration
-@ComponentScan(basePackages = "kr.co.connect.*") // @Controller 도 포함 
+@ComponentScan(basePackages = "kr.co.connect", useDefaultFilters = false, includeFilters = @ComponentScan.Filter(GroupController.class))
 @EnableAspectJAutoProxy
-@EnableWebMvc 
-public class BeanConfiguration extends WebMvcConfigurerAdapter {
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	// js, css, img 등 리소스 파일의 URL 매핑 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	// ViewResolver -> 설정 내용이 없으면 404 예외 발생 
+	@Bean
+	public BeanNameViewResolver beanNameViewResolver() {
+		BeanNameViewResolver viewResolver = new BeanNameViewResolver();
+		viewResolver.setOrder(0); // 우선순위 
+		return viewResolver;
+	}
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setOrder(1);
+		return viewResolver;
+	}
+	
+	
+	// 여기서부터 Jdbc 관련 빈들
 	
 	@Bean
 	public DriverManagerDataSource dataSource() {
@@ -53,5 +79,4 @@ public class BeanConfiguration extends WebMvcConfigurerAdapter {
 		mapperFactoryBean.setSqlSessionFactory(sqlSession);
 		return mapperFactoryBean;
 	}
-	
 }
