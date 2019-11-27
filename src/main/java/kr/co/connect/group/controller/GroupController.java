ad7ff461b8bc2676708fcec302c19ed9c2099ee0
youjin <estele.choi@gmail.com>
2019-11-27 14:00:49 +0900

    controller 인식 오류

--- src/main/java/kr/co/connect/group/controller/GroupController.java
+++ src/main/java/kr/co/connect/group/controller/GroupController.java
@@ -1,9 +1,14 @@
 package kr.co.connect.group.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +23,20 @@ public class GroupController {
 //	public GroupMapperInterface groupMapper;
 
 	// 클라이언트가 요청한 주소(url)에 대한 응답 페이지를 매핑
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+	
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	
 	@RequestMapping("/makeGroup")
 	public String makeGroup() {
 		return "groups/makeGroup"; // 실제 응답 페이지가 있는 경로
